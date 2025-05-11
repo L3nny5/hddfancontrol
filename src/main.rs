0febf53293fe715fffbcddbd6f0c6ec7a06ef0d9
L3nny5 <lennart_schwind@me.com>
2025-05-11 19:08:13 +0200

    another one

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,7 @@ fn main() -> anyhow::Result<()> {
     let log_max_size_bytes = match humantime::parse_duration(&args.log_max_size) {
         Ok(dur) => dur.as_secs(),
         Err(_) => Byte::from_str(&args.log_max_size)
-            .map(|b| b.get_bytes_as_u64())
+            .map(|b| b.get_bytes_through(byte_unit::ByteUnit::B) as u64)
             .with_context(|| format!("Invalid value for --log-max-size: {}", args.log_max_size))?,
     };
 
